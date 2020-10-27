f3022fbec6434b84ec548468eea0e74d421cc464
Bruno Willian Andrade <brunowillianandrade@gmail.com>
2020-10-27 09:01:28 -0300

    first commit

---
+++ Assets/Editor/Editor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+class Editor: EditorWindow {
+    
+    
+    static void Build() {
+
+        // Place all your scenes here
+        string[] scenes = {"Assets/scenes/c1.unity"};
+
+        string pathToDeploy = "buildAll";
+        string[] args = System.Environment.GetCommandLineArgs();
+
+        string env = "";
+        string target = "";
+        for (int i = 0; i < args.Length; i++)
+        {
+            if (args[i] == "-environment")
+            {
+                env = args[i + 1];
+            }
+
+            if (args[i] == "-target")
+            {
+                target = args[i + 1];
+            }
+        }
+
+        // GameObject game = GameObject.FindGameObjectWithTag("GameController");
+        // Game controller = game.GetComponent<Game>();
+        //
+        // controller.SetEnv(env.Contains("dev") ? Game.CustomEnum.Dev : Game.CustomEnum.Prod);
+
+        BuildPlayerOptions options = new BuildPlayerOptions();
+        options.scenes = scenes;
+        options.locationPathName = pathToDeploy;
+        options.target = target.Contains("android") ? BuildTarget.Android : BuildTarget.StandaloneWindows64;
+        BuildPipeline.BuildPlayer(options);
+        // BuildPipeline.BuildPlayer(scenes, pathToDeploy,
+        //     target.Contains("android") ? BuildTarget.Android : BuildTarget.StandaloneWindows64, BuildOptions.None);
+        //
+        //
+    }
+}
---
+++ Assets/Game.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Game:MonoBehaviour
+{
+    public enum CustomEnum // your custom enumeration
+    {
+        Prod, 
+        Dev
+    };
+    public CustomEnum dropDown = CustomEnum.Prod;  // this public var should appear as a drop down
+
+    private void Start()
+    {
+        FindObjectOfType<Text>().text = dropDown == CustomEnum.Prod ? "Production" : "Development";
+    }
+
+    public void SetEnv(CustomEnum env)
+    {
+        dropDown = env;
+        FindObjectOfType<Text>().text = dropDown == CustomEnum.Prod ? "Production" : "Development";
+    }
+}
